1a6220ab80c4c0dece5440a00aa3f8d161c8f4f0
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2021-12-21 19:46:15 -0600

    Get query params and log

---
+++ script.js
@@ -0,0 +1,15 @@
+const getQueryParams = (url) => {
+    let qParams = {};
+    // create a binding tag to use a property called search
+    let anchor = document.createElement('a');
+    // assign the href URL of the anchor tag
+    anchor.href = url;
+    // search property returns URL query string
+    let qStrings = anchor.search.substring(1);
+    let params = qStrings.split('&');
+    for (let i = 0; i < params.length; i++) {
+      let pair = params[i].split('=');
+        qParams[pair[0]] = decodeURIComponent(pair[1]);
+      }
+      return qParams;
+};
